a3d9984a0140569c15fbe8b401dbbb84fd0132d7
MyD <m.demir-21@outlook.com>
2025-05-15 15:18:59 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,6 @@
-//import { createScene } from './Game';
+import { createScene } from './Game';
 
-//const canvas = document.getElementById('webgl-canvas') as HTMLCanvasElement;
-//const scene = createScene(canvas);
+const canvas = document.getElementById('webgl-canvas') as HTMLCanvasElement;
+const scene = createScene(canvas);
 
 // İleride buraya oyun döngüsü, input kontrolleri, model yükleme vs. eklenecek
-import { Game } from './Game';
-
-const game = new Game();
-game.start();
